6b5a45001c7bc567d2ae134a330a5f186bae2116
David Nelson <david.jonathan.nelson@gmail.com>
2013-06-15 21:12:22 -0700

    milestone: got a fully rendered template header working with a workaround for bug 1459 that simulates (re-implements) transclusion.

--- src/main/webapp/js/lib/fixed-header-angular-table.js
+++ src/main/webapp/js/lib/fixed-header-angular-table.js
@@ -28,16 +28,83 @@ angular.module('fhat', [])
 
     .directive('fhatHeaderColumn', function() {
         return {
-            restrict:'EA'
+            priority: 1,
+            restrict:'EA',
+            transclude: false,
+            replace: false,
+            controller: ['$scope', 'fhatMessageBus', function($scope, fhatMessageBus) {
+                $scope.fhatMessageBus = fhatMessageBus;
+            }],
+            compile: function compile(tElement, tAttrs, transclude) {
+                return {
+                    pre: function preLink(scope, iElement, iAttrs, controller) {
+
+                    },
+                    post: function postLink(scope, iElement, iAttrs, controller) {
+                        // workaround (part 1) to issue: https://github.com/angular/angular.js/issues/1459
+                        // ( can't use a td in a template )
+                        // so, shallow (being mindful of not causing memory leaks) copy out the non $ attributes and the innerHTML
+                        var attributes = {};
+                        angular.forEach(iAttrs, function(value, key) {
+                            if(key.indexOf('$') !== 0) { // not an angular attribute
+                                attributes[key] = value; // strings should be safe to copy
+                            }
+                        });
+                        scope.fhatMessageBus.headerColumns.push({
+                            attributes: attributes,
+                            innerHTML: iElement[0].innerHTML  // strings should be safe to copy
+                        });
+                    }
+                };
+            }
         };
     })
 
-    .directive('fhatHeaderRow', function() {
+    .directive('fhatHeaderRow', function($compile) {
         return {
             restrict: 'EA',
-            replace: true,
-            template: '<div class="fhatHeaderTableContainer"><table class="fhatHeaderTable"><tbody class="fhatTableBody"><tr class="fhatHeaderRow">' +
-                '<td class="fhatHeaderColumn">Id</td><td class="fhatHeaderColumn">Name</td></tr></tbody></table></div>'
+            replace: false,
+            transclude: false,
+            controller: ['$scope', '$compile', '$interpolate', 'fhatMessageBus', function($scope, $compile, $interpolate, fhatMessageBus) {
+                $scope.fhatMessageBus = fhatMessageBus;
+                $scope.compile = $compile;
+                $scope.interpolate = $interpolate;
+                $scope.fhatMessageBus.headerColumns = [];
+            }],
+            compile: function compile(tElement, tAttrs, transclude) {
+                return {
+                    pre: function preLink(scope, iElement, iAttrs, controller) {
+
+                    },
+                    post: function postLink(scope, iElement, iAttrs, controller) {
+                        // workaround (part 2) to issue: https://github.com/angular/angular.js/issues/1459
+                        // inserting an entire table works fine, so we reassemble our rows here based on the data extracted earlier from the input dsl
+                        var columns = '';
+
+                        var convertObjectToAttributes = function(attributesAsObject) {
+                            var attributesAsString = '';
+
+                            angular.forEach(attributesAsObject, function(value, key) {
+                                attributesAsString = ' ' + key + '\"' + value + '\" ';
+                            });
+
+                            return attributesAsString;
+                        };
+
+                        angular.forEach(scope.fhatMessageBus.headerColumns, function(headerColumn, index) {
+                            columns += '<td class="fhatHeaderColumn" ' + convertObjectToAttributes(headerColumn.attributes) + '>' +
+                            headerColumn.innerHTML +
+                             '</td>';
+                        });
+
+                        var nonInterpolatedTemplate = '<table class="fhatHeaderTable"><tr class="fhatHeaderRow">' + columns + '</table></tr>';
+                        var interpolationFunction = scope.interpolate(nonInterpolatedTemplate);
+                        var interpolatedTemplate = interpolationFunction(scope);
+
+                        iElement.replaceWith(interpolatedTemplate);
+                    }
+                };
+            }
         };
     })
 
