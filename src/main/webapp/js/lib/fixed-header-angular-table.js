8e8754bc058259450cf0baf534deda9197ae3e7c
David Nelson <david.jonathan.nelson@gmail.com>
2013-06-17 00:09:55 -0700

    milestone: selected cell highlighting.

--- src/main/webapp/js/lib/fixed-header-angular-table.js
+++ src/main/webapp/js/lib/fixed-header-angular-table.js
@@ -6,7 +6,7 @@ angular.module('fhat', [])
             restrict: 'E',
             // manually transclude and replace the template to work around not being able to have a template with td or tr as a root element
             // see bug: https://github.com/angular/angular.js/issues/1459
-            compile: function compile(tElement, tAttrs) {
+            compile: function (tElement, tAttrs) {
                 // find whatever classes were passed into the fhat, and merge them with the built in classes for the container div
                 tElement.addClass('fhatTableContainer');
 
@@ -19,7 +19,6 @@ angular.module('fhat', [])
             }
         };
     })
-
     .directive('fhatHeaderRow', ['fhatManualCompiler', function(fhatManualCompiler) {
         return {
             // only support elements for now to simplify the manual transclusion and replace logic.  see below.
@@ -27,40 +26,41 @@ angular.module('fhat', [])
             restrict: 'E',
             controller: ['$scope', '$parse', function($scope, $parse) {
 
-                $scope.handleClick = function(row, parentScopeClickHandler) {
-                    var clickHandlerFunctionName = parentScopeClickHandler.replace('(row)', '');
-
-                    $scope.$parent[clickHandlerFunctionName](row);
-                };
             }],
             // manually transclude and replace the template to work around not being able to have a template with td or tr as a root element
             // see bug: https://github.com/angular/angular.js/issues/1459
-            compile: function compile(tElement, tAttrs) {
+            compile: function (tElement, tAttrs) {
                 fhatManualCompiler.compileRow(tElement, tAttrs, true);
             }
         };
     }])
-
-    .directive('fhatRow', ['fhatManualCompiler', function(fhatManualCompiler) {
+    .directive('fhatRow', ['fhatManualCompiler', 'fhatMessageBus', function(fhatManualCompiler, fhatMessageBus) {
         return {
             // only support elements for now to simplify the manual transclusion and replace logic.  see below.
             // this kills IE8< support for now, which is fine as that's not a use case that this directive is initially solving.
             restrict: 'E',
             controller: ['$scope', '$parse', function($scope, $parse) {
-                $scope.handleClick = function(row, parentScopeClickHandler) {
+                $scope.handleClick = function(row, parentScopeClickHandler, selectedRowBackgroundColor) {
                     var clickHandlerFunctionName = parentScopeClickHandler.replace('(row)', '');
+
+                    fhatMessageBus.previouslySelectedRow.backgroundColor = fhatMessageBus.previouslySelectedRowColor;
+
+                    row.backgroundColor = selectedRowBackgroundColor;
+
+                    fhatMessageBus.previouslySelectedRow = row;
 
                     $scope.$parent[clickHandlerFunctionName](row);
                 };
             }],
             // manually transclude and replace the template to work around not being able to have a template with td or tr as a root element
             // see bug: https://github.com/angular/angular.js/issues/1459
-            compile: function compile(tElement, tAttrs) {
+            compile: function (tElement, tAttrs) {
+                fhatMessageBus.rowSelectedBackgroundColor = tAttrs.selectedColor;
+
                 fhatManualCompiler.compileRow(tElement, tAttrs, false);
             }
         };
     }])
-
     .service('fhatManualCompiler', function() {
         var self = this;
 
@@ -94,7 +94,8 @@ angular.module('fhat', [])
             } else {
                 // add the ng-repeat and row selection click handler to each row
                 rowTemplate = rowTemplate.replace('<tr',
-                    '<tr ng-repeat="row in model" ng-click="handleClick(row, \'' + tAttrs.onSelected + '\')" ');
+                    '<tr ng-repeat="row in model" style="background-color: {{ row.backgroundColor }}" ng-click="handleClick(row, \'' +
+                        tAttrs.onSelected + '\', \'' + tAttrs.selectedColor + '\')" ');
             }
 
             // wrap our rows in a table, and a container div.  the container div will manage the scrolling.
@@ -107,6 +108,10 @@ angular.module('fhat', [])
 
     .service('fhatMessageBus', function() {
         var self = this;
+
+        // store a reference to the previously selected row so we can access it without looking it up from the bound model
+        self.previouslySelectedRow = {};
+        self.previouslySelectedRowColor = '';
 
         return self;
     });
