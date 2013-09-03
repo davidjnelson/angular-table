ac3c14260d4e636f1e0e421614170126991b8999
David Nelson <david.jonathan.nelson@gmail.com>
2013-09-02 18:16:30 -0700

    new version of angular-table website

---
+++ node_modules/webserver/webserver.js
@@ -0,0 +1,41 @@
+var
+  path = require('path'),
+  http = require('http'),
+  paperboy = require('paperboy'),
+
+  PORT = 8003,
+  WEBROOT = ".";
+
+http.createServer(function(req, res) {
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .addHeader('Expires', 300)
+    .addHeader('X-PaperRoute', 'Node')
+    .before(function() {
+    })
+    .after(function(statCode) {
+      log(statCode, req.url, ip);
+    })
+    .error(function(statCode, msg) {
+		res.writeHead(statCode, {'Content-Type': 'text/plain'});
+		res.end("Error " + statCode);
+		log(statCode, req.url);
+    })
+    .otherwise(function(err) {
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end("Error 404: File not found");
+		log(404, req.url);
+    });
+}).listen(PORT);
+
+function log(statCode, url, ip, err) {
+  var logStr = statCode + ' - ' + url + ' - ' + ip;
+  if (err)
+    logStr += ' - ' + err;
+  console.log(logStr);
+}
+
+console.log("WEBROOT "+WEBROOT);
+console.log("Listening @ "+PORT);
+
